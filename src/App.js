60dbda68e978aab0cbe0821bc04367d613f75611
Ram-saddist <nagulavancharavisivaramteja@gmail.com>
2024-06-21 16:24:45 +0530

    home & add product

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter,Routes,Route } from 'react-router-dom'
 import Navigation from './Navigation/Navigation'
 import Signup from './Signup/Signup'
 import Login from './Login/Login'
+import AddProduct from './AddProduct/AddProduct'
+import Home from './Home/Home'
 
 export default function App() {
   return (
@@ -10,8 +12,10 @@ export default function App() {
       <BrowserRouter>
         <Navigation/>
         <Routes>
+          <Route path='/' element={<Home/>}/>
           <Route path="/login" element={<Login/>}/>
           <Route path="/signup" element={<Signup/>}/>
+          <Route path='/add-product' element={<AddProduct/>}/>
         </Routes>
       </BrowserRouter>
     </div>
